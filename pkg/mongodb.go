c486b24edcbc4f38041205b10231d85969c0d340
Ben Hundley <ben.hundley@gmail.com>
2016-06-23 11:39:06 -0700

    removing unneeded comments

--- pkg/mongodb.go
+++ pkg/mongodb.go
@@ -20,9 +20,6 @@ func configureReplicaSet(instances []*client.InstanceResource) error {
 	rsConf := `{_id: "rs0", members: [` + strings.Join(rsConfMems, ", ") + `]}` // TODO TODO TODO rs name....... param
 
 	primaryAddr := instances[0].Addresses.Internal[0].Address
-	// cmd := exec.Command("mongo", primaryAddr)
-	// cmd.Stdin = strings.NewReader("rs.initiate(); rs.reconfig(" + rsConf + ")\n")
-	// return cmd.Run()
 
 	initCmd := exec.Command("mongo", primaryAddr)
 	initCmd.Stdin = strings.NewReader("rs.initiate()\n")
