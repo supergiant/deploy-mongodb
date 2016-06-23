b03b2abdb63e2cb9d514dda64aa0b8caf2d5ed28
Ben Hundley <ben.hundley@gmail.com>
2016-06-23 11:32:16 -0700

    finalized and working

--- pkg/deploy.go
+++ pkg/deploy.go
@@ -4,7 +4,7 @@ import supergiant "github.com/supergiant/supergiant/client"
 
 func Deploy(appName *string, componentName *string) error {
 
-	supergiant.Log.SetLevel("debug")
+	// supergiant.Log.SetLevel("debug")
 
 	sg := supergiant.New("http://supergiant-api.supergiant.svc.cluster.local/v0", "", "", true)
 
@@ -59,46 +59,15 @@ func Deploy(appName *string, componentName *string) error {
 	}
 	currentInstances := currentList.Items
 
-	// es := newEsClient(component.Addresses.External[0].Address)
-	//
-	// if err := es.waitForShardRecovery(); err != nil {
-	// 	return err
-	// }
-
 	// remove instances
 	if currentRelease.InstanceCount > targetRelease.InstanceCount {
-
-		// if err := es.setMinMasterNodes(targetRelease.InstanceCount/2 + 1); err != nil {
-		// 	return err
-		// }
-
 		instancesRemoving := currentRelease.InstanceCount - targetRelease.InstanceCount
-
-		// var awarenessAttrs []string
-		// for i := instancesRemoving; i > 0; i-- {
-		// 	id := *currentInstances[len(currentInstances)-i].ID
-		// 	attr := "n" + id
-		// 	awarenessAttrs = append(awarenessAttrs, attr)
-		// }
-		// if err := es.setAwarenessAttrs(awarenessAttrs); err != nil {
-		// 	return err
-		// }
-		// if err := es.waitForShardRecovery(); err != nil {
-		// 	return err
-		// }
 
 		for _, instance := range currentInstances[len(currentInstances)-instancesRemoving:] {
 			if err := instance.Stop(); err != nil {
 				return err
 			}
-			// if err := es.waitForShardRecovery(); err != nil {
-			// 	return err
-			// }
 		}
-
-		// if err := es.clearAwarenessAttrs(); err != nil {
-		// 	return err
-		// }
 
 		// add new instances
 	} else if currentRelease.InstanceCount < targetRelease.InstanceCount {
@@ -115,10 +84,6 @@ func Deploy(appName *string, componentName *string) error {
 			}
 		}
 	}
-
-	// if err := es.disableShardRebalancing(); err != nil {
-	// 	return err
-	// }
 
 	// update instances
 
@@ -149,35 +114,12 @@ func Deploy(appName *string, componentName *string) error {
 		currentInstance := currentInstances[i]
 		targetInstance := targetInstances[i]
 
-		// if err := es.disableShardAllocation(); err != nil {
-		// 	return err
-		// }
-		//
-		// if err := es.flushTranslog(); err != nil {
-		// 	return err
-		// }
-
 		currentInstance.Stop()
 		currentInstance.WaitForStopped()
 
 		targetInstance.Start()
 		targetInstance.WaitForStarted()
-
-		// assertNodeConnected()
-		// time.Sleep(30 * time.Second)
-
-		// if err := es.enableShardAllocation(); err != nil {
-		// 	return err
-		// }
-		//
-		// if err := es.waitForShardRecovery(); err != nil {
-		// 	return err
-		// }
 	}
-
-	// if err := es.enableShardRebalancing(); err != nil {
-	// 	return err
-	// }
 
 	return configureReplicaSet(targetInstances)
 }
